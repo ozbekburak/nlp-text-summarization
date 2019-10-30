2b4421a56a0e4655a976605ab5c834b13fdcfc75
Burak Kaan <ozbekburakkaan@outlook.com>
2019-10-30 13:52:19 +0300

    Suriye-Turkiye savasi hakkinda 5 adet haber birlestirilerek eklendi.

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ from nltk.stem import PorterStemmer
 
 nltk.download('punkt')
 nltk.download('stopwords')
+
+tokenize = nltk.data.load('tokenizers/punkt/PY3/english.pickle')
+with open('/home/gregorsamsa/Masters Degree/Natural Language Processing/nlp-text-summarizer/NewsAll_Turkey-Syria.txt') as newsfile:
+    text = newsfile.read()
