519b3f2ad6ac822df4c25e0023ceeba2260e00d8
Burak Kaan <ozbekburakkaan@outlook.com>
2019-10-30 13:57:46 +0300

    tokenizing islemleri yapildi

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-nltk.download('punkt')
-nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
-tokenize = nltk.data.load('tokenizers/punkt/PY3/english.pickle')
 with open('/home/gregorsamsa/Masters Degree/Natural Language Processing/nlp-text-summarizer/NewsAll_Turkey-Syria.txt') as newsfile:
     text = newsfile.read()
+
+# Metini cumlelee boluyoruz.
+tokenizer = nltk.data.load('tokenizers/punkt/PY3/english.pickle')
+tokenized_to_sentence = tokenizer.tokenize(text)
+
+# Kelime kelime bolmek icin on hazirlik
+regex_tokenizer = RegexpTokenizer("[\w']+")
+tokenized_to_word = regex_tokenizer.tokenize(''.join(tokenized_to_sentence))
