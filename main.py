67741f49e36473a4872bda328eaef85e08a7a85a
Burak Kaan <ozbekburakkaan@outlook.com>
2019-11-06 10:53:03 +0300

    [refactoring] add pre_processing method

--- main.py
+++ main.py
@@ -23,3 +23,29 @@ import numpy as np
 
 with codecs.open('Summaries/Group1/Group1_News_All.txt', 'r', encoding='utf-8', errors='ignore') as group1_news_all:
     document = group1_news_all.read()
+
+# tokenization
+tokenizer = nltk.data.load('tokenizers/punkt/PY3/english.pickle')
+tokenized_to_sentence = tokenizer.tokenize(document)
+
+regex_tokenizer = RegexpTokenizer("[\w']+")
+tokenized_to_word = regex_tokenizer.tokenize(''.join(tokenized_to_sentence))
+
+
+def pre_processing(text):
+    # deleting stopwords
+    english_stops = set(stopwords.words('english'))
+    tokenized_words_without_stopwords = []
+    for word in tokenized_to_word:
+        if word not in english_stops:
+            tokenized_words_without_stopwords.append(word)
+
+    # stemming operation
+    stemmer = PorterStemmer()
+    tokenized_words_without_stopwords_and_stemmed = []
+    for word in tokenized_words_without_stopwords:
+        tokenized_words_without_stopwords_and_stemmed.append(stemmer.stem(word))
+
+    return tokenized_words_without_stopwords_and_stemmed
+
+
